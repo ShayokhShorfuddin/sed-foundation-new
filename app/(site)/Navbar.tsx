5ebfefbdc78b3c8251f3e977529221b207b1379c
Shayokh <shayokhshuvro14@gmail.com>
2024-09-07 09:08:05 +0600

    add chevron down icon next to News & Events menu, add hover display for News & Event links and accordion for mobile

--- app/(site)/Navbar.tsx
+++ app/(site)/Navbar.tsx
@@ -70,16 +70,18 @@ export default function Navbar() {
               <li>
                 <Link href={"/"}>Home</Link>
               </li>
+
               <li>
                 <Link href={"/"}>About</Link>
               </li>
+
               <li>
                 <Accordion type="single" collapsible className="w-full">
                   <AccordionItem value="item">
                     <AccordionTrigger className="text-left">
-                      <div className="flex">
+                      <div className="inline-flex items-center">
                         <p className="mr-2">Programs</p>
-                        <ChevronDownIcon />
+                        <ChevronDownIcon size={13} />
                       </div>
                     </AccordionTrigger>
 
@@ -101,9 +103,28 @@ export default function Navbar() {
                   </AccordionItem>
                 </Accordion>
               </li>
+
               <li>
-                <Link href={"/"}>News & Events</Link>
+                <Accordion type="single" collapsible className="w-full">
+                  <AccordionItem value="item">
+                    <AccordionTrigger className="text-left">
+                      <div className="inline-flex items-center">
+                        <p className="mr-2">News & Events</p>
+                        <ChevronDownIcon size={13} />
+                      </div>
+                    </AccordionTrigger>
+
+                    <AccordionContent className="text-left mt-4 ml-4">
+                      <Link href={"/news"}>News</Link>
+                    </AccordionContent>
+
+                    <AccordionContent className="text-left mt-4 ml-4">
+                      <Link href={"/events"}>Events</Link>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
               </li>
+
               <li>
                 <NavbarDialogButton />
               </li>
@@ -168,18 +189,18 @@ export function Navlinks() {
               </div>
             </HoverCardTrigger>
 
-            <HoverCardContent className="w-fit">
+            <HoverCardContent className="w-fit p-2">
               <div className="z-50">
                 <Link href={"/climate-action"}>
-                  <p className="py-1 px-3 hover:bg-accent">Climate Action</p>
+                  <p className="py-1 px-2 hover:bg-accent">Climate Action</p>
                 </Link>
                 <Link href={"/research-publication"}>
-                  <p className="py-1 px-3 hover:bg-accent">
+                  <p className="py-1 px-2 hover:bg-accent">
                     Research & Publications
                   </p>
                 </Link>
                 <Link href={"/environmental-autumn-school"}>
-                  <p className="py-1 px-3 hover:bg-accent">
+                  <p className="py-1 px-2 hover:bg-accent">
                     Environmental Autumn School
                   </p>
                 </Link>
@@ -189,12 +210,25 @@ export function Navlinks() {
         </NavigationMenuItem>
 
         <NavigationMenuItem>
-          <NavigationMenuLink
-            asChild
-            className="mr-4 md:mr-7 hover:text-sedGreen transition duration-160 ease-in-out"
-          >
-            <Link href={"/"}>News & Events</Link>
-          </NavigationMenuLink>
+          <HoverCard>
+            <HoverCardTrigger className="mr-4 md:mr-5 hover:text-sedGreen transition duration-160 ease-in-out">
+              <div className="inline-flex justify-center items-center">
+                <p className="mr-2">News & Events</p>
+                <ChevronDownIcon size={15} />
+              </div>
+            </HoverCardTrigger>
+
+            <HoverCardContent className="w-fit p-2">
+              <div className="z-50">
+                <Link href={"/news"}>
+                  <p className="py-1 px-3 hover:bg-accent">News</p>
+                </Link>
+                <Link href={"/events"}>
+                  <p className="py-1 px-3 hover:bg-accent">Events</p>
+                </Link>
+              </div>
+            </HoverCardContent>
+          </HoverCard>
         </NavigationMenuItem>
 
         <NavigationMenuItem>
