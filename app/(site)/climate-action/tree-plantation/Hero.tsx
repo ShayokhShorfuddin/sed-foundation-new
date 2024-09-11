b7f93b0d59af91ee9eedc2277e175b9aaa059233
Shayokh <shayokhshuvro14@gmail.com>
2024-09-11 20:56:34 +0600

    add responsive tree plantation page

---
+++ app/(site)/climate-action/tree-plantation/Hero.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Tree from "./_images/tree-plantation.svg";
+
+export default function Hero() {
+  return (
+    <section className="container mt-4 md:mt-5">
+      <div className="flex flex-col md:flex-row justify-around items-center font-outfit">
+        <div className="flex flex-col max-w-[30rem] pr-5 sm:text-center md:text-left">
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl">
+            <span className="text-sedGreen">Tree</span> Plantation
+            <span className="text-sedGreen">.</span>
+          </h1>
+
+          <p className="mt-4 md:mt-5 font-sans">
+            Our organization is committed to advancing tree plantation efforts.
+            We recognize that tree planting serves as a pivotal strategy in
+            addressing climate change, rehabilitating natural ecosystems, and
+            enhancing the well-being of communities globally. Through our
+            initiatives focused on tree plantation, we aspire to foster a more
+            sustainable and healthier environment for future generations.
+          </p>
+        </div>
+
+        <Image
+          src={Tree}
+          alt="Tree"
+          className="size-[20rem] lg:size-[32rem] mt-6 md:mt-0"
+        />
+      </div>
+    </section>
+  );
+}
---
+++ app/(site)/climate-action/tree-plantation/Info.tsx
@@ -0,0 +1,102 @@
+import Image from "next/image";
+import Dhaka1 from "./_images/dhaka1.jpg";
+import Dhaka2 from "./_images/dhaka2.jpg";
+import Dhaka3 from "./_images/dhaka3.jpg";
+
+export default function Info() {
+  return (
+    <section className="container mb-20">
+      <Section1 />
+      <Section2 />
+      <Section3 />
+    </section>
+  );
+}
+
+function Section1() {
+  return (
+    <section className="mt-10">
+      <div className="flex flex-col-reverse lg:flex-row justify-around items-center sm:text-center lg:text-left">
+        <Image
+          src={Dhaka2}
+          alt="Dhaka2"
+          className="max-w-full sm:max-w-[35rem] lg:mr-10 mt-10 lg:mt-0"
+        />
+
+        <div className="flex flex-col max-w-[30rem]">
+          <h1 className="text-3xl lg:text-4xl">
+            A <span className="text-[#a7a279]">city</span> with challenges
+            <span className="text-sedGreen">.</span>
+          </h1>
+
+          <p className="mt-4">
+            Dhaka, the capital city of Bangladesh, faces a critical
+            environmental crisis despite its significance as a major urban
+            center. With a forest cover of less than 5-6% of its total area, the
+            city ranks as one of the most densely populated worldwide, leaving
+            limited space for traditional afforestation.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function Section2() {
+  return (
+    <section className="mt-24">
+      <div className="flex flex-col lg:flex-row justify-around items-center sm:text-center lg:text-left">
+        <div className="flex flex-col max-w-[30rem]">
+          <h1 className="text-3xl lg:text-4xl">
+            The <span className="text-sedGreen">CFAD</span> program
+            <span className="text-sedGreen">.</span>
+          </h1>
+
+          <p className="mt-4">
+            Recognizing the urgent need for environmental improvement, the SED
+            Foundation has initiated the "Circle Forestation around Dhaka
+            (CFAD)" program. This initiative aims to increase the city's green
+            cover through a comprehensive approach that involves social and
+            public participation.
+          </p>
+        </div>
+
+        <Image
+          src={Dhaka1}
+          alt="Dhaka1"
+          className="max-w-full sm:max-w-[35rem] lg:ml-10 mt-10 lg:mt-0"
+        />
+      </div>
+    </section>
+  );
+}
+
+function Section3() {
+  return (
+    <section className="mt-24">
+      <div className="flex flex-col-reverse lg:flex-row justify-around items-center sm:text-center lg:text-left">
+        <Image
+          src={Dhaka3}
+          alt="Dhaka3"
+          className="max-w-full sm:max-w-[35rem] lg:mr-10 mt-10 lg:mt-0"
+        />
+
+        <div className="flex flex-col max-w-[30rem]">
+          <h1 className="text-3xl lg:text-4xl">
+            A <span className="text-sedGreen">ray</span> of hope
+            <span className="text-sedGreen">.</span>
+          </h1>
+
+          <p className="mt-4">
+            The CFAD program will focus on the districts surrounding Dhaka,
+            including Narayanganj, Gazipur, Narsingdi, Manikganj, Munsiganj, and
+            Faridpur. By expanding forestation efforts in these areas, the SED
+            Foundation seeks to create a greener belt around the capital city,
+            mitigating environmental hazards and improving the overall quality
+            of life for residents.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
