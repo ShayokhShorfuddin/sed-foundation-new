fef4b0a60f18bf77af1f798eab2273de1a61e15a
Shayokh <shayokhshuvro14@gmail.com>
2024-09-19 21:26:33 +0600

    add research articles

--- app/(site)/research-publication/Programs.tsx
+++ app/(site)/research-publication/Programs.tsx
@@ -26,7 +26,7 @@ export default function Programs() {
 
     {
       image: Research2,
-      href: "/",
+      href: "/research-publication/research-articles",
       title: "Research Articles",
       description:
         "Forests cover hills with tall trees. Rivers flow through green valleys. Plants grow in fields and by lakes. Mountains rise high, with snowy tops. Animals live in woods and grasslands. Flowers bloom in meadows. Oceans have sandy beaches. Deserts are hot and dry. Rainforests are wet and full of life.",
