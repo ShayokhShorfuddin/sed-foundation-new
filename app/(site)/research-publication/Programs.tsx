adc57b7b3d7a6f50419972f4fe90e7b118a3d5fc
Shayokh <shayokhshuvro14@gmail.com>
2024-09-19 22:16:39 +0600

    add newsletter page

--- app/(site)/research-publication/Programs.tsx
+++ app/(site)/research-publication/Programs.tsx
@@ -50,7 +50,7 @@ export default function Programs() {
 
     {
       image: Newsletter,
-      href: "/",
+      href: "/research-publication/newsletter",
       title: "Newsletter",
       description:
         "Forests cover hills with tall trees. Rivers flow through green valleys. Plants grow in fields and by lakes. Mountains rise high, with snowy tops. Animals live in woods and grasslands. Flowers bloom in meadows. Oceans have sandy beaches. Deserts are hot and dry. Rainforests are wet and full of life.",
