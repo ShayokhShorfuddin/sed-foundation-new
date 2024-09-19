82eebef6593e4ed5b2794032001c3fdb6c699ae3
Shayokh <shayokhshuvro14@gmail.com>
2024-09-19 20:24:10 +0600

    update moot court tabs and combobox

--- app/(site)/climate-action/moot-court/Info.tsx
+++ app/(site)/climate-action/moot-court/Info.tsx
@@ -17,6 +17,49 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
+const Participants = () => <p>Participants go here</p>;
+const Awardees = () => <p>Awardees go here</p>;
+const JudgesGuests = () => <p>Judges go here</p>;
+const BenchMemorial = () => <p>I have no idea what that is lol.</p>;
+
+const tabOptions = [
+  {
+    key: "participants",
+    title: "Participants",
+    content: <Participants />,
+  },
+  {
+    key: "awardees",
+    title: "Awardees",
+    content: <Awardees />,
+  },
+  {
+    key: "judges-guests",
+    title: "Judges & guests",
+    content: <JudgesGuests />,
+  },
+  {
+    key: "bench-memorial",
+    title: "Bench memorial",
+    content: <BenchMemorial />,
+  },
+];
+
+const comboOptions = [
+  { value: "participants", label: "Participants", component: <Participants /> },
+  { value: "awardees", label: "Awardees", component: <Awardees /> },
+  {
+    value: "judges-guests",
+    label: "Judges & guests",
+    component: <JudgesGuests />,
+  },
+  {
+    value: "bench-memorial",
+    label: "Bench memorial",
+    component: <BenchMemorial />,
+  },
+];
+
 export default function Info() {
   return (
     <section className="container mb-20">
@@ -30,21 +73,11 @@ function TabSection() {
   return (
     <section className="hidden sm:flex flex-col justify-center items-center">
       <Tabs aria-label="Options" size="md">
-        <Tab key="participants" title="Participants">
-          <p>Participants go here</p>
-        </Tab>
-
-        <Tab key="awardees" title="Awardees">
-          <p>Awardees go here</p>
-        </Tab>
-
-        <Tab key="judges-guests" title="Judges & guests">
-          <p>Judges go here</p>
-        </Tab>
-
-        <Tab key="bench-memorial" title="Bench memorial">
-          <p>I have no idea what that is lol.</p>
-        </Tab>
+        {tabOptions.map((option) => (
+          <Tab key={option.key} title={option.title}>
+            {option.content}
+          </Tab>
+        ))}
       </Tabs>
     </section>
   );
@@ -57,38 +90,17 @@ function ComboSection() {
     <section className="flex sm:hidden flex-col justify-center items-center">
       <ComboboxDemo onValueChange={setSelectedValue} />
 
-      {selectedValue === "participants" && (
-        <p className="mt-5">Participants go here</p>
-      )}
-      {selectedValue === "awardees" && <p className="mt-5">Awardees go here</p>}
-      {selectedValue === "judges-guests" && (
-        <p className="mt-5">Judges go here</p>
-      )}
-      {selectedValue === "bench-memorial" && (
-        <p className="mt-5">I have no idea what that is lol.</p>
+      {selectedValue && (
+        <div className="mt-5">
+          {
+            comboOptions.find((option) => option.value === selectedValue)
+              ?.component
+          }
+        </div>
       )}
     </section>
   );
 }
-
-const options = [
-  {
-    value: "participants",
-    label: "Participants",
-  },
-  {
-    value: "awardees",
-    label: "Awardees",
-  },
-  {
-    value: "judges-guests",
-    label: "Judges & guests",
-  },
-  {
-    value: "bench-memorial",
-    label: "Bench memorial",
-  },
-];
 
 function ComboboxDemo({
   onValueChange,
@@ -108,7 +120,7 @@ function ComboboxDemo({
           className="w-full max-w-[200px] justify-between"
         >
           {value
-            ? options.find((option) => option.value === value)?.label
+            ? comboOptions.find((option) => option.value === value)?.label
             : "Select option"}
           <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
@@ -118,7 +130,7 @@ function ComboboxDemo({
         <Command>
           <CommandList>
             <CommandGroup>
-              {options.map((option) => (
+              {comboOptions.map((option) => (
                 <CommandItem
                   key={option.value}
                   value={option.value}
