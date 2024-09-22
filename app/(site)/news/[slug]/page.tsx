b7bc5f8152ca19217c74e4dd5e7b221b2d9b2849
Shayokh <shayokhshuvro14@gmail.com>
2024-09-22 07:17:47 +0600

    setup the news page

--- app/(site)/news/[slug]/page.tsx
+++ app/(site)/news/[slug]/page.tsx
@@ -1,13 +1,41 @@
+import Navbar from "../../Navbar";
 import { getNewsBySlug } from "@/sanity/sanity-utils";
 import { PortableText } from "next-sanity";
+import { NewsType } from "../NewsSection";
 
 type Props = {
   params: { slug: string };
 };
 
 export default async function Page({ params }: Props) {
-  const slug = params.slug;
-  const news = await getNewsBySlug(slug);
+  const news = await getNewsBySlug(params.slug);
 
-  return <p>Hello</p>;
+  return (
+    <main>
+      <Navbar />
+      <NewsContent news={news} />
+    </main>
+  );
+}
+
+function NewsContent({ news }: { news: NewsType }) {
+  return (
+    <section className="container flex flex-col max-w-[65rem] mt-5 mb-10">
+      <img
+        src={news.cardImage.asset.url}
+        alt={news.cardImage.alt}
+        className="rounded-xl"
+      />
+
+      <h1 className="text-3xl mt-8">{news.title}</h1>
+
+      <span className="px-3 py-1 border border-sedGreen w-min rounded-full text-sm mt-5">
+        {new Intl.DateTimeFormat("en-GB").format(new Date(news.date))}
+      </span>
+
+      <div className="mt-10">
+        <PortableText value={news.content} />
+      </div>
+    </section>
+  );
 }
