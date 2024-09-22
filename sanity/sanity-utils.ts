bd00bf3860192a4bcd4345465e7280afb203762f
Shayokh <shayokhshuvro14@gmail.com>
2024-09-22 08:23:57 +0600

    add utils for events

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,7 +1,7 @@
 import { NewsType } from "@/app/(site)/news/NewsSection";
 import { createClient, groq } from "next-sanity";
 
-export async function getProjects(): Promise<NewsType[]> {
+export async function getNews(): Promise<NewsType[]> {
   const client = createClient({
     projectId: "q99ib49k",
     dataset: "production",
@@ -35,6 +35,51 @@ export async function getNewsBySlug(slug: string): Promise<NewsType> {
     title,
     "slug": slug.current,
     content,
+    "cardImage": card_image{asset->{url}, alt},
+    date
+  }`,
+    { slug }
+  );
+}
+
+export async function getEvents(): Promise<NewsType[]> {
+  const client = createClient({
+    projectId: "q99ib49k",
+    dataset: "production",
+
+    apiVersion: "2024-09-21", // Use current date (UTC format yyyy-mm-dd)
+  });
+
+  return client.fetch(groq`*[_type == "events"]{
+    _id,
+    _createdAt,
+    title,
+    "slug": slug.current,
+    content,
+    venue,
+    location_on_google_maps,
+    "cardImage": card_image{asset->{url}, alt},
+    date
+  }`);
+}
+
+export async function getEventBySlug(slug: string): Promise<NewsType> {
+  const client = createClient({
+    projectId: "q99ib49k",
+    dataset: "production",
+
+    apiVersion: "2024-09-21", // Use current date (UTC format yyyy-mm-dd)
+  });
+
+  return client.fetch(
+    groq`*[_type == "events"]{
+    _id,
+    _createdAt,
+    title,
+    "slug": slug.current,
+    content,
+    venue,
+    location_on_google_maps,
     "cardImage": card_image{asset->{url}, alt},
     date
   }`,
