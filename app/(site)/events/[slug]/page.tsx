c6770d91eb7c62de06dbc4447f94a31487af45e1
Shayokh <shayokhshuvro14@gmail.com>
2024-09-23 09:38:05 +0600

    update slug event page

--- app/(site)/events/[slug]/page.tsx
+++ app/(site)/events/[slug]/page.tsx
@@ -40,23 +40,52 @@ function EventContent({ event }: { event: EventsType }) {
 
       <h1 className="text-lg md:text-xl lg:text-2xl mt-8">{event.title}</h1>
 
-      <div className="flex items-center mt-5">
-        <p className="px-3 py-1 border border-sedGreen w-min rounded-full text-sm mr-3">
-          {event.from_date}
-          {/* {new Intl.DateTimeFormat("en-GB").format(new Date(event.date))} */}
-        </p>
-
-        <div className="flex text-gray-800">
-          <MapPin className="mr-2" />
-          <Link href={event.location_on_google_maps} target="_blank">
-            {event.venue}
-          </Link>
-        </div>
+      <div className="flex flex-col xs:flex-row xs:justify-between xs:items-center mt-4 flex-wrap">
+        <Dates fromDate={event.from_date} toDate={event.to_date} />
+        <Location
+          venue={event.venue}
+          location_on_google_maps={event.location_on_google_maps}
+        />
       </div>
 
       <div className="mt-10">
         <PortableText value={event.content} components={components} />
       </div>
     </section>
+  );
+}
+
+function Location({
+  venue,
+  location_on_google_maps,
+}: {
+  venue: string;
+  location_on_google_maps: string;
+}) {
+  return (
+    <div className="flex text-gray-800 mt-4 xs:mt-0">
+      <MapPin className="mr-2" />
+      <Link href={location_on_google_maps} target="_blank">
+        {venue}
+      </Link>
+    </div>
+  );
+}
+
+function Dates({ fromDate, toDate }: { fromDate: Date; toDate: Date }) {
+  return (
+    <div className="flex">
+      <p className="px-3 py-1 border border-sedGreen w-min rounded-full text-sm mr-3">
+        {/* DD/MM/YYYY */}
+        {new Intl.DateTimeFormat("en-GB").format(new Date(fromDate))}
+      </p>
+
+      {new Date(toDate) > new Date(fromDate) && (
+        <p className="px-3 py-1 border border-red-400 w-min rounded-full text-sm">
+          {/* DD/MM/YYYY */}
+          {new Intl.DateTimeFormat("en-GB").format(new Date(toDate))}
+        </p>
+      )}
+    </div>
   );
 }
