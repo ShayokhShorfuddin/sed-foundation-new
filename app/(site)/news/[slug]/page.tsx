f79d6b6082335ce78dfe3a31f34fc73054197aa8
Shayokh <shayokhshuvro14@gmail.com>
2024-09-23 18:12:20 +0600

    update news page

--- app/(site)/news/[slug]/page.tsx
+++ app/(site)/news/[slug]/page.tsx
@@ -38,6 +38,7 @@ function NewsContent({ news }: { news: NewsType }) {
 
       <h1 className="text-lg md:text-xl lg:text-2xl mt-8">{news.title}</h1>
 
+      {/* DD/MM/YYYY */}
       <p className="px-3 py-1 border border-sedGreen w-min rounded-full text-sm mt-5">
         {new Intl.DateTimeFormat("en-GB").format(new Date(news.date))}
       </p>
