f7c1e2796832a60a6dc783e2bc66f2ad6bbe05d7
Shayokh <shayokhshuvro14@gmail.com>
2024-09-25 09:00:31 +0600

    add use client to Archive

--- app/(site)/research-publication/journal/Archive.tsx
+++ app/(site)/research-publication/journal/Archive.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { getIssues } from "@/sanity/utils/issues-utils";
 import Image from "next/image";
 import Link from "next/link";
---
+++ app/(site)/research-publication/journal/[slug]/page.tsx
@@ -0,0 +1,23 @@
+import Navbar from "@/app/(site)/Navbar";
+import { getIssueWithDetails } from "@/sanity/utils/issues-utils";
+import { IssuesType } from "../Archive";
+
+type Props = {
+  params: { slug: string };
+};
+
+export default async function Page({ params }: Props) {
+  const issue = await getIssueWithDetails(params.slug);
+
+  return (
+    <main>
+      <Navbar />
+      <IssuesContent issue={issue} />
+    </main>
+  );
+}
+
+// TODO: Error
+function IssuesContent({ issue }: { issue: IssuesType }) {
+  return <p>{issue.title}</p>;
+}
