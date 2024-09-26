db361cb9eddf77f8323bbb21a17001906584913d
Shayokh <shayokhshuvro14@gmail.com>
2024-09-26 07:53:53 +0600

    update Metatags for image

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,21 @@ export const metadata: Metadata = {
   title: "SED | Strategy for Environmental Development Foundation",
   description:
     "Strategy for Environmental Development (SED) is founded in Bangladesh in light of the motto “Save the Environment, Save Your Existence”. Bangladesh is the country currently affected heavily from the cause of climate change. Also, some man-made causes damage the environment of the Country. Global warming and change of weather patterns originated many complications and affected both directly and indirectly the whole environment with its ecology.",
+
+  openGraph: {
+    title: "SED | Strategy for Environmental Development Foundation",
+    description:
+      "Strategy for Environmental Development (SED) is founded in Bangladesh in light of the motto “Save the Environment, Save Your Existence”. Bangladesh is the country currently affected heavily from the cause of climate change. Also, some man-made causes damage the environment of the Country. Global warming and change of weather patterns originated many complications and affected both directly and indirectly the whole environment with its ecology.",
+    url: "https://sedbd.org",
+    siteName: "SED | Strategy for Environmental Development Foundation",
+    images: [
+      {
+        url: "https://sed-foundation.vercel.app/logo.jpg",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
